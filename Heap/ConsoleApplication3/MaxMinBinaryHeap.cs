6ca023340d3a33ce9ec2b0ce8c2a918e6cdb76fd
Koval1997 <kovalmax06@gmail.com>
2017-04-26 20:00:15 +0500

    Add files via upload

---
+++ Heap/ConsoleApplication3/MaxMinBinaryHeap.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{    
+
+    public class MaxMinBinaryHeap<T>  where T : IComparable<T>
+    {
+        List <T> items;
+        
+        public MaxMinBinaryHeap()
+        {
+            items = new List<T>();            
+        }
+
+        public void HeapBuilder(bool flag, params T[] sourceArray)
+        {
+            items = sourceArray.ToList();
+            for (int i = 0; i < Size; i++)
+            {
+                if (items.Contains(sourceArray[i])) throw new Exception("Содержит повторяющиеся элементы!");
+            }
+            for (int i = items.Count / 2; i >= 0; i--)
+            {
+                heapify(i, flag);
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+
+        public void Insert(T item, bool flag)
+        {
+            if (items.Contains(item)) throw new Exception("Есть такой элемент!");            
+            items.Add(item);
+            for (int i = Size / 2; i >= 0; i--)
+            {
+                heapify(i,flag);
+            }
+        }  
+
+        public T Extract(bool flag)
+        {
+             
+            if (items.Count == 0) throw new Exception("Куча пуста!");           
+            T resault = items[0];        
+            items.RemoveAt(0);
+            for (int i = Size / 2; i >= 0; i--)
+            {
+                heapify(i, flag);
+            }
+            return resault;
+        }
+
+        public void heapify(int i, bool flag)
+        {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+            int largest = i;
+
+            if (leftIndex < Size)
+            {
+                if ((items[leftIndex].CompareTo(items[largest]) > 0)^flag)
+                    largest = leftIndex;
+            }
+            if (rightIndex < Size)
+            {
+                if ((items[rightIndex].CompareTo(items[largest]) > 0)^flag)
+                    largest = rightIndex;
+            }
+            if (largest != i)
+            {
+                T temp = items[largest];
+                items[largest] = items[i];
+                items[i] = temp;
+                i = largest;
+            }
+        }      
+
+    }
+}
---
+++ Heap/ConsoleApplication3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adr = new MaxMinBinaryHeap<int>();
+            adr.HeapBuilder(true, 8,5,2,3,1,4,9,6,0,7);
+            
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(adr.Extract(true));
+            }                
+            Console.ReadLine();
+        }
+    }
+}
